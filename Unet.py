5e18a4388caa4b36d9f4f9f0cebc4b97a18b4a61
Vishakha Vijay Atole <43256657+vishcocity@users.noreply.github.com>
2019-03-03 18:52:53 -0500

    Unet+ zero padding

---
+++ Unet.py
@@ -0,0 +1,169 @@
+import cv2
+import numpy as np
+import os
+
+dir = "/Users/vishakhaatole/Desktop/DSP/proj2data"
+
+train_file = open(dir+"/"+"train.txt",)
+
+train_names = []
+for data in train_file:
+    split_data = data.split()[0]
+    train_names.append(split_data)
+
+print(train_names)
+
+def zero_padding(input_array, final_dimension = [640,640]):
+    input_array_size = input_array.shape
+    if input_array_size[0] != final_dimension[0] or input_array_size[1] != final_dimension[1]:
+        temp = np.zeros((final_dimension[0],final_dimension[1]))
+        temp[:input_array_size[0],:input_array_size[1]] = input_array
+        return temp
+    else:
+        return input_array
+
+import tarfile
+import scipy.misc
+
+for names in train_names:
+    img_file = tarfile.open(dir + "/" + "data" + "/" + names + ".tar")
+    mask_file = dir + "/" + "masks" + "/" + names + ".png"
+    #getting preprocessed mask images
+    mask_imgs = cv2.imread(mask_file,0)
+    mask_imgs = zero_padding(mask_imgs)
+    np.save(dir + "/" + "padding_intermediate_masks" + "/" + names + ".npy", mask_imgs)
+    #getting preprocessed train images
+    #extracting 1 frames from each tar file
+    extracted_img = img_file.getnames()[0:1]
+    for img in extracted_img:
+        imgs = np.asarray(bytearray(img_file.extractfile(img).read()),dtype ="uint8")
+        imgs = cv2.imdecode(imgs, cv2.IMREAD_GRAYSCALE)
+        imgs = zero_padding(imgs)
+        #saving the intermediate arrays in a folder for future use
+        np.save(dir + "/" + "padding_intermediate" + "/" + names + ".npy", imgs)
+
+import numpy as np
+import os
+import numpy as np
+from keras.models import *
+from keras.layers import *
+from keras.optimizers import *
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+from keras import backend as keras
+from keras.preprocessing.image import ImageDataGenerator
+
+def unet(pretrained_weights = None,input_size = (256,256,1)):
+    inputs = Input(input_size)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    drop4 = Dropout(0.5)(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
+
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    drop5 = Dropout(0.5)(conv5)
+
+    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    merge6 = concatenate([drop4,up6], axis = 3)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
+
+    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    merge7 = concatenate([conv3,up7], axis = 3)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
+
+    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    merge8 = concatenate([conv2,up8], axis = 3)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
+
+    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    merge9 = concatenate([conv1,up9], axis = 3)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
+
+    model = Model(input = inputs, output = conv10)
+
+    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    #model.summary()
+
+    if(pretrained_weights):
+    	model.load_weights(pretrained_weights)
+
+    return model
+
+model = unet()
+
+model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+
+def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image_color_mode = "grayscale",
+                    mask_color_mode = "grayscale",image_save_prefix  = "image",mask_save_prefix  = "mask",
+                    flag_multi_class = False,num_class = 2,save_to_dir = None,target_size = (256,256),seed = 1):
+    '''
+    can generate image and mask at the same time
+    use the same seed for image_datagen and mask_datagen to ensure the transformation for image and mask is the same
+    if you want to visualize the results of generator, set save_to_dir = "your path"
+    '''
+    print("!")
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+    image_generator = image_datagen.flow_from_directory(
+        train_path,
+        classes = [image_folder],
+        class_mode = None,
+        color_mode = image_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = image_save_prefix,
+        seed = seed)
+    mask_generator = mask_datagen.flow_from_directory(
+        train_path,
+        classes = [mask_folder],
+        class_mode = None,
+        color_mode = mask_color_mode,
+        target_size = target_size,
+        batch_size = batch_size,
+        save_to_dir = save_to_dir,
+        save_prefix  = mask_save_prefix,
+        seed = seed)
+    train_generator = zip(image_generator, mask_generator)
+    for (img,mask) in train_generator:
+        img,mask = adjustData(img,mask,flag_multi_class,num_class)
+        yield (img,mask)
+
+data_gen_args = dict(rotation_range=0.2,
+                    width_shift_range=0.05,
+                    height_shift_range=0.05,
+                    shear_range=0.05,
+                    zoom_range=0.05,
+                    horizontal_flip=True,
+                    fill_mode='nearest')
+myData = trainGenerator(2,dir,dir+"/"+'padding_intermediate',dir+"/"+'padding_intermediate_masks',data_gen_args)#,save_to_dir = dir+"/"+"fun")
+
+x_train =[]
+y_train= []
+def load_from_files():
+    for name in train_names:
+        train_x = np.load(os.path.join(dir,"padding_intermediate/"+ name + ".npy"))
+        train_y = np.load(os.path.join(dir, "padding_intermediate_masks/"+ name + ".npy"))
+        x_train.append(train_x)
+        y_train.append(train_y)
+    return x_train, y_train
+
+train,mask = load_from_files()
+#print(train.shape)
+model.fit(train,mask,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
