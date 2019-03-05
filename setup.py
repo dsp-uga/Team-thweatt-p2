6de36405f28b43921f38868cff7db30dfe38c8b7
Saed Rezayi <saed.rezaei@gmail.com>
2019-03-05 18:08:58 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", 'r') as fp:
+	long_description = fp.read()
+
+setuptools.setup(
+	name = "Thweatt-p1",
+	version = "1.0.0",
+	author="Saed Rezayi, Hemanth Dandu, Vishakha Atole, Abhishek Chatrath",
+	author_email="saedr@uga.edu, hemanthme22@gmail.com, va45686@uga.edu, ac06389@uga.edu",
+	license='MIT',
+	description="A package for cilia segmentation task.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/dsp-uga/Team-thweatt-p2",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+	],
+	test_suite='nose.collector',
+	tests_require=['nose'],
+    install_requires=['keras'],
+)
